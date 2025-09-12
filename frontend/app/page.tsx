a557fc48cad6828a329c5a347876b4e8348ac93c
Richard Leyshon <richard.leyshon@justice.gov.uk>
2025-09-12 14:14:49 +0100

    chore: Rm debugging statements

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,10 +3,6 @@ import React from "react";
 import MainParentComponent from "@/components/main-component";
 
 export default function Home() {
-  console.log("HELLO MATE INSIDE PAGE");
-  console.log("process.env INSIDE PAGE", process.env.NEXT_PUBLIC_API_URL);
-  console.log("API_BASE_URL INSIDE PAGE", process.env.NEXT_PUBLIC_API_URL);
-
   return (
     <div className="">
       <MainParentComponent />
